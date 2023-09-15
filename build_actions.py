ef2ac73cb2c8e000121d1da973825cc82f860975
Vince Burns <vburns@sensata.com>
2023-09-15 14:59:24 -0500

    Remove Counting of colors in each individual card.
    This will be done a layer up to ensure we don't ever miss it.

--- build_actions.py
+++ build_actions.py
@@ -1,42 +1,34 @@
 #Each function takes in the player that built it, the opposing player, and the game instance. 
 def single_wood_build(game, build_player):
     build_player.n_wood += 1
-    build_player.n_brown += 1
     return
 
 def single_brick_build(game, build_player):
     build_player.n_brick += 1
-    build_player.n_brown += 1
     return
 
 def single_stone_build(game, build_player):
     build_player.n_stone += 1
-    build_player.n_brown += 1
     return
 
 def single_glass_build(game, build_player):
     build_player.n_glass += 1
-    build_player.n_grey += 1
     return
 
 def single_paper_build(game, build_player):
     build_player.n_paper += 1
-    build_player.n_grey += 1
     return
 
 def double_wood_build(game, build_player):
     build_player.n_wood += 2
-    build_player.n_brown += 1
     return
 
 def double_brick_build(game, build_player):
     build_player.n_brick += 2
-    build_player.n_brown += 1
     return
 
 def double_stone_build(game, build_player):
     build_player.n_stone += 2
-    build_player.n_brown += 1
     return
 
 def piraeus(game, build_player):

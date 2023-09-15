a24895cb5e2a87448c4ad0419570077f72c54757
Vince Burns <vburns@sensata.com>
2023-09-15 15:00:13 -0500

    Add some more end_game actions that are needed

--- end_game_action.py
+++ end_game_action.py
@@ -1,20 +1,23 @@
 #Each function takes in the player that built it, the opposing player, and the game instance. 
-def add_3_points(game):
+def add_1_points(game):
     build_player = game.active_player()
-    build_player.n_points += 3 
-
-def add_9_points(game):
-    build_player = game.active_player()
-    build_player.n_points += 9 
-
-def add_6_points(game):
-    build_player = game.active_player()
-    build_player.n_points += 6 
+    build_player.n_points += 1 
 
 def add_2_points(game):
     build_player = game.active_player()
     build_player.n_points += 2 
 
+def add_3_points(game):
+    build_player = game.active_player()
+    build_player.n_points += 3 
+
+def add_6_points(game):
+    build_player = game.active_player()
+    build_player.n_points += 6 
+
+def add_9_points(game):
+    build_player = game.active_player()
+    build_player.n_points += 9 
 
 def no_action(game):
     return
